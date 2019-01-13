506e4d17b8ad212135eec0c658d70698201b628c
Dumindu de Silva <dumindudesilva083@gmail.com>
2019-01-13 12:02:10 +0530

    MYB-100 Added models

---
+++ src/main/java/com/sample/model/Student.java
@@ -0,0 +1,11 @@
+package com.sample.model;
+
+public class Student
+{
+    private int id;
+    private String firstname;
+    private String lastname;
+
+
+
+}
---
+++ src/main/java/com/sample/model/Subject.java
@@ -0,0 +1,9 @@
+package com.sample.model;
+
+public class Subject {
+
+    private int subjectId;
+    private String title;
+    private String description;
+
+}
